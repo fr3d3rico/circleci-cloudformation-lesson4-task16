2b13881be025c01ef228a6bd19f8c4b3fa904e5c
Fred <frederico.a.soares@gmail.com>
2021-07-24 13:05:33 -0300

    task 18

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.end("Hello world, Fred! Now the process is updated after kill it.");
+res.end("Hello world!");
 });
 server.listen(3000);
