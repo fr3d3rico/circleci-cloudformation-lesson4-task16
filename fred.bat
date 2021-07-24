e1cd198b11b53b4a026ca75f9dd21e57032a8993
Fred <frederico.a.soares@gmail.com>
2021-07-24 12:35:03 -0300

    task 18

--- fred.bat
+++ fred.bat
@@ -1,3 +1,5 @@
+echo [all] > inventory
+aws ec2 --profile devops describe-instances --query "Reservations[*].Instances[*].PublicIpAddress" --output text >> inventory
 git add *
 git commit -m "task 18"
 git push origin task18
